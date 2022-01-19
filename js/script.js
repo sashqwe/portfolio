51c179c4c7a299e6982c399a9cea601429db1795
sashqwe <87759522+sashqwe@users.noreply.github.com>
2022-01-19 15:00:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+// menu
+
+  const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      over = document.querySelector('.overlay'),
+      first = document.querySelector('.line_first'),
+      second = document.querySelector('.line_second'),
+      third = document.querySelector('.line_third');
+
+  hamburger.addEventListener('click', () => {
+    over.classList.toggle('overlay_active'),
+    hamburger.classList.toggle('hamburger_active'),
+    menu.classList.toggle('menu_active'),
+    first.classList.toggle('top'),
+    second.classList.toggle('off'),
+    third.classList.toggle('bottom');
+  });
+  
+// modals
+
+  const overlay = document.querySelector('.overlay'); 
+  const formButtons = document.querySelectorAll('.form__button');
+  const successButton = document.querySelector('.main__button');
+  const form = document.querySelector('.modal__form');
+  const success = document.querySelector('.modal__succesfull'); 
+  const closeButtons = document.querySelectorAll('.modal__close'); 
+
+  successButton.addEventListener('click', (e) => { 
+    e.preventDefault(); 
+    overlay.classList.add('overlay_active');
+    form.classList.add('modal__form_active');
+    success.classList.remove('modal__succesfull_active');
+     
+  })
+
+  formButtons.forEach((button) => {
+    button.addEventListener('click', (e) => { 
+    overlay.classList.add('overlay_active'); 
+    form.classList.remove('modal__form_active');
+    success.classList.add('modal__succesfull_active');
+    })
+  })
+
+  closeButtons.forEach((close) =>{
+    close.addEventListener('click', (e) => { 
+    e.preventDefault(); 
+    overlay.classList.remove('overlay_active'); 
+    form.classList.remove('modal__form_active');
+    success.classList.remove('modal__succesfull_active');
+    })
+  })
+
+// slider
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3, 
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: false, 
+    prevArrow: '<button type="button" class="slick-prev"><img class="prev" src="icons/arrow-left.svg"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img class="next" src="icons/arrow-right.svg"></img></button>',
+    responsive: [
+      {
+        breakpoint: 577,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }]
+  });
+
+  // mailer
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+       type:"POST",
+       url: "./mailer/smart.php",
+       data: $(this).serialize()
+    }).done(function(){
+       $(this).find("input").val("");
+
+       $('form').trigger('reset');
+    });
+    return false;
+});
+
+});
---
+++ mailer/smart.php
@@ -0,0 +1,46 @@
+<?php 
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'as.zeliuk@gmail.com';                 // Наш логин
+$mail->Password = 'yfjellepupygswyr';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('as.zeliuk@gmail.com', 'mySite');   // От кого письмо 
+$mail->addAddress('sashke.semax@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . ' <br>
+	Сообщение: ' . $message . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
